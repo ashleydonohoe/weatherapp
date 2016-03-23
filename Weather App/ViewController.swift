7182188668c36c1af91a39da0424fcae8dee9df6
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-03-23 00:29:33 -0400

    have city search working

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var searchbox: UITextField!
     var city = ""
+    var weatherData: String!
     
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,7 +39,24 @@ class ViewController: UIViewController {
         print("Getting Data for \(city)")
         
         let url = NSURL(string:"http://www.weather-forecast.com/locations/\(city)/forecasts/latest")!
-        print(url)
+        
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                if websiteArray!.count > 0 {
+                    let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                    print(weatherArray[0])
+                    if weatherArray.count > 0 {
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.resultLabel.text = weatherSummary
+                        })
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 }
-
